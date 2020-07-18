a152804832637d9622e969e2e328dc37d54d1e90
RandDarras <randdarras@yahoo.com>
2020-07-18 09:13:06 +0300

    Nav-Bar Created

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
+import "./style.css";
+import Nav from "./components/navBar";
+import List from "./components/list";
+import AddItem from "./components/addItem";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-const App = () => {
-  return <h1>App</h1>;
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Route path="/" exact component={Home} />
+          <Route path="/addItem" component={AddItem} />
+          <Route path="/list" component={List} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+var Home = () => {
+  return (
+    <div>
+      <br /> <br />
+      <label>Enter Your UserName : </label>
+      <input
+        type="text"
+        // onChange={onChange}
+        placeholder="Enter your name"
+        // value={user.username}
+      />
+      <br /> <br />
+      <label>$ In Your Wallet : </label>
+      <input
+        type="text"
+        // onChange={onChange}
+        placeholder="$ in your wallet"
+        // value={user.wallet}
+      />
+    </div>
+  );
 };
 
 export default App;
