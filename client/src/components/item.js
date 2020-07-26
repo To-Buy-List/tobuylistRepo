ba9f32d3c3bc0578d30428f54d55c8316c5adae4
RandDarras <randdarras@yahoo.com>
2020-07-26 15:31:41 +0300

    Price required in add item

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -1,19 +1,13 @@
 import React from "react";
 
 const Item = (props) => {
-  // to render if it has price or not
-  const formLi = () => {
-    if (!props.item.price) return <li>{props.item.item}</li>;
-    else
-      return (
-        <li>
-          {props.item.item}
-          <br />
-          Price .. {props.item.price} JD
-        </li>
-      );
-  };
-  return <div>{formLi()}</div>;
+  return (
+    <li>
+      {props.item.item}
+      <br />
+      Price .. {props.item.price} JD
+    </li>
+  );
 };
 
 export default Item;
