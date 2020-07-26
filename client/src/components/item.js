93ab1a79c0da6e0a670ec251b34e37194fe4055d
RandDarras <randdarras@yahoo.com>
2020-07-26 14:37:17 +0300

    Edited item component to render price

--- client/src/components/item.js
+++ client/src/components/item.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Item = (props) => {
+  // to render if it has price or not
   const formLi = () => {
     if (!props.item.price) return <li>{props.item.item}</li>;
     else
