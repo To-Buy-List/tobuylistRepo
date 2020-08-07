ff8870df5e50516189b6bbb3e25e7841942dacab
RandDarras <randdarras@yahoo.com>
2020-08-07 19:44:35 +0300

    Deploying

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
+const path = require("path");
 require("dotenv").config();
 
 //to set an env on your local mashine run export PORT=
 const port = process.env.PORT || 5100;
 
-//serve up static assets (on heroku)
+//serve up static assets if in production(on heroku)
 if (process.env.NODE_ENV === "production") {
+  //set a static folder
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 app.use(cookieParser());
